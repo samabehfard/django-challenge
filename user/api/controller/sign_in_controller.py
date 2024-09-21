31fe4cef78ae1ba4f08d26e16eefb6e8c50247e9
sama <raphichigo@gmail.com>
2024-09-22 02:30:03 +0330

    ADD: add swagger to sign_in and sign_up controller

--- user/api/controller/sign_in_controller.py
+++ user/api/controller/sign_in_controller.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseBadRequest
 from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,6 +6,7 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
+from common.common_errors import BadRequestException
 from common.common_serializer import CommonSerializer
 from user.api.serializer.sign_in_serializer import UserLoginSerializer, UserLoginResponseSerializer
 
@@ -23,7 +25,7 @@ class SignInView(APIView):
         try:
             serializer = UserLoginSerializer(request.data)
             if not serializer.is_valid():
-                return Response({"detail": "bad request"}, status=status.HTTP_400_BAD_REQUEST)
+                raise BadRequestException("bad request")
 
             username = serializer.data.get('username')
             password = serializer.data.get('password')
@@ -32,6 +34,8 @@ class SignInView(APIView):
                 token, created = Token.objects.get_or_create(user=user)
                 return Response({'token': token.key}, status=status.HTTP_200_OK)
             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        except BadRequestException:
+            return Response("bad request", status=status.HTTP_400_BAD_REQUEST)
         except Exception as error:
             # error.args[0] should be log
             return Response({'detail': 'Internal Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- user/api/controller/signup_controller.py
+++ user/api/controller/signup_controller.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponseBadRequest
+from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from common.common_errors import BadRequestException
+from common.common_serializer import CommonSerializer
 from user.api.serializer.signup_serializer import UserSignUpSerializer
 from user.errors import DuplicateUserNameException, DuplicateIdentityNumberException
 from user.logic.user_authentication_logic import UserAuthenticationLogic
@@ -12,12 +15,20 @@ class SignUpView(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.user_authentication_logic = UserAuthenticationLogic()
-
+    @extend_schema(
+        request=UserSignUpSerializer,
+        responses={
+            status.HTTP_200_OK: CommonSerializer,
+            status.HTTP_400_BAD_REQUEST: CommonSerializer,
+            status.HTTP_500_INTERNAL_SERVER_ERROR: CommonSerializer
+        },
+        description="end point for login."
+    )
     def post(self, request):
         try:
             serializer = UserSignUpSerializer(data=request.data)
             if not serializer.is_valid():
-                raise HttpResponseBadRequest("bad request")
+                raise BadRequestException("bad request")
             username = serializer.data.get('username')
             name = serializer.data.get('name')
             family_name = serializer.data.get('family_name')
@@ -32,8 +43,8 @@ class SignUpView(APIView):
                 phone_number=phone_number,
                 password=password
             )
-            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
-        except HttpResponseBadRequest:
+            return Response({'detail': 'User created successfully'}, status=status.HTTP_200_OK)
+        except BadRequestException:
             return Response("bad request", status=status.HTTP_400_BAD_REQUEST)
         except DuplicateIdentityNumberException:
             # we should log this but we should not show our existed identity numbers to other users
